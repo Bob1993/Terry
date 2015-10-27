389768edf1f86f0d14647bd7fb01f0c3d209bcc6
bob <bob1993.dev@gmail.com>
2015-10-28 00:29:24 +0800

    first commit

---
+++ Task2.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+/**
+ * +的时候指針一直往右移动,(第一行)直到12个码完成(12个+),再重头开始。
+ * 当第一行出现-的时候指針往下移动,当出现连续2个-,-时候都重头开始,
+ * 当第二行出现+时指針返回第一行右邉第一个数字
+ * @author Bob
+ */
+public class Task2 {
+	static int i,j,k,re;//i,j定义当前指针位置,	k操作符数组当前所在位置,	re返回结果值
+	static boolean descReset= false;//控制双－号出现是是否重置的标记量
+	static char[] cs;//接收输入符
+	static int[][] source= {//源数据
+			{2, 3 ,4,  5, 7, 9, 12, 18, 24, 30, 42, 54},
+			{4, 6, 8, 10, 14 ,18, 24, 36, 48, 60, 84, 108}
+			};
+	
+	public static void descMove()//-操作的移动规则
+	{		
+			if (!descReset ) {
+				i++;
+				if (k< cs.length-1&&cs[k+1]=='-') {
+					descReset= !descReset;
+				}
+			}else {
+				reset();
+				descReset= !descReset;
+			}
+	}
+	
+	public static void addMove()//+操作的移动规则
+	{
+		if ( j == source[0].length-1) {
+			reset();
+		}else if ( i == 1){
+			j++;	i--;
+		}else j++;	
+	}
+	
+	public static void reset(){
+		i= 0; j= 0;
+	}
+	
+	/**
+	 * 暂时先处理掉所有的空格，因为java里，会将空格和回车作为输入结束的标识
+	 * @param str
+	 */
+	public static void filter(String str){
+		str.replace(" ", "");
+	}
+	
+	public static void operate(String str)
+	{
+		filter(str);
+		cs= str.toCharArray();
+		for (;k < cs.length; k++) {
+			if(cs[k]== '-'){	
+				System.out.printf("-%d", source[i][j]);
+				re-= source[i][j];
+				descMove();
+			}else if(cs[k]== '+'){
+				System.out.printf("+%d", source[i][j]);
+				re+= source[i][j];
+				addMove();
+			}
+			else{
+				 System.out.println("\nERROR_INPUT");
+				 return ;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		String str= null;
+		while((str= input.next())!= null)
+		{
+			re= 0; i= 0; j= 0;	k= 0; descReset= false;//再次输入时，将六个参数置0；
+			operate(str);
+		}
+		input.close();
+	}
+}
---
+++ Task3.java
@@ -0,0 +1,148 @@
+import java.util.Scanner;
+/**
+ * 本次任务描述：根据上次的代码结构来根据四行b or o输入转移后的＋—来操作源数据
+ *第一条：b为＋，o为－
+ *第二条：b为－，o为＋
+ *第三条：与前一符号不同为＋，相同为－
+ *第四条：相同为＋，不同为－
+ * source[i][j];  为指针所在的下一个预测值
+ * 
+ * @author Bob
+ */
+public class Task3  extends Task2{
+	static String inputString, translatedString;
+	static int total;
+	static char[] cs;
+	static boolean isPredict;
+	
+	/**
+	 * ��b o���и���4�ֲ�ͬ�Ĺ���ת��Ϊ4�ֽ������������taask2��ִ��
+	 */
+	public static char rule1(char c){
+		if (c== 'b') {
+			translatedString+= '+';
+			return '+';
+		}else {
+			translatedString+= '-';
+			return '-';
+		}
+	}
+	
+	public static char rule2(char c){
+		if (c== 'o') {
+			translatedString+= '+';
+			return '+';
+		}else{
+			translatedString+= '-';
+			return '-';
+		} 
+	}
+	
+	public static char rule3(int i, char c){
+		if (isPredict) {
+			if (c== cs[cs.length-1]) {
+				return '-';
+			}else return '+';
+		}
+		
+		if (i>  cs.length-1) {
+			return ' ';
+		}
+		if (cs[i-1]!= cs[i]) {
+			translatedString+= '+';
+			return '+';
+		}else {
+			translatedString+= '-';
+			return '-';
+		}
+	}
+	
+	public static char rule4(int i, char c){
+		if (isPredict) {
+			if (c== cs[cs.length-1]) {
+				return '+';
+			}else return '-';
+		}
+		
+		if (i > cs.length-1) {
+			return ' ';
+		}
+		if (cs[i]== cs[i-1]) {
+			translatedString+= '+';
+			return '+';
+		}else {
+			translatedString+= '-';
+			return '-';
+		}
+	}
+	
+	/**
+	 * 
+	 * @param rule Ԥ��Ĺ����
+	 * @param c Ԥ����ַ�ֵ
+	 * @return Ԥ����
+	 */
+	public static char predict(int rule, char c){
+		isPredict= true;
+		switch (rule) {
+		case 1:
+			return rule1(c);
+		case 2:
+			return rule2(c);
+		case 3:
+			return rule3(0, c);
+		case 4:
+			return rule4(0, c);
+		default: return ' ';
+		}
+	}
+	
+	public static void rules(int rule){
+		resetParams();
+		System.out.printf("\n����%d\n", rule);
+		for(int i= 0; i< cs.length; i++ ){
+			switch (rule) {
+			case 1:
+				rule1(cs[i]);
+				break;
+			case 2:
+				rule2(cs[i]);
+				break;
+			case 3:
+				rule3(i+1, ' ');
+				break;
+			case 4:
+				rule4(i+1, ' ');
+				break;
+			default: break;
+			}
+		}
+		System.out.println(translatedString);
+		Task2.operate(translatedString);
+		System.out.printf("\n= %d\t Next:\tb%s%d   o%s%d", re, predict(rule, 'b'),  source[i][j], predict(rule, 'o') , source[i][j]);
+	}
+	
+	public static void translate(){
+		cs= inputString.toCharArray();
+		for (int i = 1; i < 5; i++) {
+			rules(i);
+			total+= re;
+		}
+		System.out.println("\n�ܺͣ�"+(total));
+		total= 0;
+	}
+	
+	public static void resetParams(){
+		re= 0; i= 0; j= 0;	k= 0; descReset= false;
+		translatedString= ""; isPredict= false;
+	}
+
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		while((inputString= input.next())!= null)
+		{
+			translate();
+		}
+		input.close();
+	}
+}
